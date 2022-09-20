47725b9512969ee76da59643c4fa45fb133979d9
luismlopes1002 <luismlopes1002@gmail.com>
2022-09-20 16:12:37 +0100

    app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { userState } from "react";
+import axios from "axios";
 
 function App() {
+  //const url = 'https://api.openweathermap.org/data/2.5/weather?id=2267056&APPID=de68f1e62679e41335ce198453b7db51'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Lisbon</p>
+          </div>
+          <div className="temp">
+            <h1>27ºC</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">27ºC</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">57%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">11 KMH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
