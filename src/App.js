f5dfd21458810a1a3aed801234c326ede618ee19
luismlopes1002 <luismlopes1002@gmail.com>
2022-09-26 18:52:53 +0100

    tentativa de update nos dados recebidos

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import axios from "axios";
 
 function App() {
   const [res, setResultArray] = useState([]);
+  const [time, setTime] = useState("");
   const APIKEY = "de68f1e62679e41335ce198453b7db51";
-
-  // const url1 = `https://api.openweathermap.org/data/2.5/weather?id=2735941&units=metric&APPID=${APIKEY}`; // PORTO
-  // const url2 = `https://api.openweathermap.org/data/2.5/weather?id=2740636&units=metric&APPID=${APIKEY}`; // COIMBRA
-  // const url3 = `https://api.openweathermap.org/data/2.5/weather?id=2267094&units=metric&APPID=${APIKEY}`; // LEIRIA
-  // const url4 = `https://api.openweathermap.org/data/2.5/weather?id=2267056&units=metric&APPID=${APIKEY}`; // LISBOA
-  // const url5 = `https://api.openweathermap.org/data/2.5/weather?id=2268337&units=metric&APPID=${APIKEY}`; // FARO
 
   const url1 = `https://api.openweathermap.org/data/2.5/weather?APPID=${APIKEY}&units=metric&q=PORTO`;
   const url2 = `https://api.openweathermap.org/data/2.5/weather?APPID=${APIKEY}&units=metric&q=COIMBRA`;
@@ -18,23 +13,49 @@ function App() {
   const url5 = `https://api.openweathermap.org/data/2.5/weather?APPID=${APIKEY}&units=metric&q=FARO`;
 
   const r1 = axios.get(url1);
-  const r2 = axios.get(url2);
-  const r3 = axios.get(url3);
-  const r4 = axios.get(url4);
-  const r5 = axios.get(url5);
+  // const r2 = axios.get(url2);
+  // const r3 = axios.get(url3);
+  // const r4 = axios.get(url4);
+  // const r5 = axios.get(url5);
 
   function getData() {
-    axios.all([r1, r2, r3, r4, r5]).then(
+    axios.all([r1]).then(
       axios.spread((...responses) => {
         setResultArray(responses);
+        setTime(new Date(responses[0].data.dt * 1000).toLocaleString());
       })
     );
+  }
+
+  function updateData() {
+    getData();
+    console.log("1st get");
+    setInterval(() => {
+      setResultArray([]);
+      setTime("");
+      console.log("Deleted data");
+      setTimeout(
+        function () {
+          getData();
+          console.log("Data gotten");
+          console.log("5m passed");
+        }.bind(this),
+        1000
+      );
+    }, 1000 * 60 * 5);
+  }
+
+  function renderSwitch(param) {
+    switch (param) {
+      case param < 10:
+        return;
+    }
   }
 
   return (
     <div className="app">
       <div className="search">
-        <button onClick={getData}>get data</button>
+        <button onClick={updateData}>get data</button>
       </div>
 
       <div className="outerContainer">
@@ -90,6 +111,9 @@ function App() {
           
         )} */}
       </div>
+      <footer className="footer">
+        {res[0] !== undefined && <p>Last Updated: {time}</p>}
+      </footer>
     </div>
   );
 }
